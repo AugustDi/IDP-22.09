d379f8f0d1c18088df3a155527313e3e24f71d20
Dmitrii Danilov <dmitrii.danilov.triare@gmail.com>
2022-09-21 12:57:33 +0300

    feature: add taskFirst

--- app/src/main/java/com/triare/idp_trainee_junior/MainActivity.kt
+++ app/src/main/java/com/triare/idp_trainee_junior/MainActivity.kt
@@ -15,6 +15,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        taskFirst()
     }
 
+    private fun taskFirst() {
+        //Use keywords val and var.
+        // val -> Immutable (read-only) local variables are defined using the val keyword. They can only be assigned a value once.
+        val immutableX: Int = 1
+        val immutableY = 1
+        Log.d("Task 1", "immutableX = $immutableX")
+        Log.d("Task 1", "immutableY = $immutableY")
+
+        //var -> Mutable variables are declared using the var keyword.
+        var mutableX: String = "for example"
+        Log.d("Task 1", "mutableX = $mutableX")
+
+        mutableX = "something else"
+        Log.d("Task 1", "immutableX = $mutableX")
+
+        //Use nullable and non-nullable variables.
+        val nullableX: Int? = null // nullable variable
+        val nonNullableX: Int = 123
+        //Use functions with and without data return.
+        withReturn()
+        Log.d("Task 1", "functions with return: return = ${withoutReturn()} ")
+        withoutReturn()
+    }
+
+    private fun withReturn(): Int {
+        return 3
+    }
+
+    private fun withoutReturn(): Unit {
+        Log.d("Task 1", ": Unit is unnecessary ")
+    }
 }
