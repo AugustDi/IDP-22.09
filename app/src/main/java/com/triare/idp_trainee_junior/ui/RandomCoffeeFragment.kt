6545c78ada3b77abcc43138fe1d756906e9eff4f
Dmitrii Danilov <dmitrii.danilov.triare@gmail.com>
2022-09-22 16:25:39 +0300

    feature: add random coffee img

--- app/src/main/java/com/triare/idp_trainee_junior/ui/RandomCoffeeFragment.kt
+++ app/src/main/java/com/triare/idp_trainee_junior/ui/RandomCoffeeFragment.kt
@@ -1,14 +1,22 @@
 package com.triare.idp_trainee_junior.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import by.kirich1409.viewbindingdelegate.CreateMethod
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.triare.idp_trainee_junior.databinding.FragmentRandomCoffeBinding
+import com.triare.idp_trainee_junior.extensions.load
+import com.triare.idp_trainee_junior.ui.dvo.RandomCoffeeResponseDvo
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 class RandomCoffeeFragment : Fragment() {
 
@@ -23,8 +31,50 @@ class RandomCoffeeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        subscribeUi()
+        initUi()
     }
+
+    private fun subscribeUi() {
+        vm.loadingState.onEach {
+            Log.d("LoadingState", "$it")
+            showProgress(it.isLoading)
+        }.launchIn(lifecycleScope)
+    }
+
+    private fun showProgress(isLoading: Boolean) {
+        binding.progress.visibility = if (isLoading) VISIBLE else GONE
+    }
+
+    private fun initUi() {
+        observeCoffeeUrl()
+        initAction()
+    }
+
+    private fun observeCoffeeUrl() {
+        vm.url.onEach {
+            loadCoffee(it?.url)
+            if (it?.url.isNullOrBlank()) showEmptyView(true) else showEmptyView(false)
+            Log.d("URL:", "${it?.url}")
+        }.launchIn(lifecycleScope)
+    }
+
+    private fun initAction() {
+        binding.action.setOnClickListener {
+            vm.getRandomCoffeeUrl()
+        }
+    }
+
+
+    private fun showEmptyView(isVisible: Boolean) {
+        binding.emptyView.visibility = if (isVisible) VISIBLE else GONE
+        binding.image.visibility = if (!isVisible) VISIBLE else GONE
+    }
+
+    private fun loadCoffee(url: String?) {
+        binding.image.load(url)
+    }
+
     companion object {
         fun newInstance() = RandomCoffeeFragment()
     }
